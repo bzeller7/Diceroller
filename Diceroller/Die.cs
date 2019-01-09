e4bf5c750709aca52319ca0900fa98b37796ef1e
bzeller7 <zeller8964@student.cptc.edu>
2019-01-09 14:19:48 -0800

    Add project files.

---
+++ Diceroller/Die.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diceroller
+{
+    /// <summary>
+    /// Represents a standard individual 
+    /// 6-sided die
+    /// </summary>
+    class Die
+    {
+        /// <summary>
+        /// Get/Set the face value of the die
+        /// </summary>
+
+        public byte Value { get; set; }
+
+        ///<summary>
+        /// Rolls the dice,
+        /// and sets the value
+        /// </summary>
+        
+        public void Roll()
+        {
+            Random rand = new Random();
+            int result = rand.Next(1, 7);
+            this.Value = (byte)result;
+        }
+    }
+}
---
+++ Diceroller/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diceroller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Die> dice = new List<Die>();
+            for (int i = 0; i < 5; i++)
+            {
+                dice.Add(new Die());
+            }
+
+            foreach (Die d in dice)
+            {
+                Console.WriteLine("Rolled a " + d.Value);
+            }
+            
+            //Die dice1 = new Die();
+            ////dice1.Roll();
+
+            //Console.WriteLine("You rolled " + dice1.Value);
+            Console.ReadKey();
+        }
+    }
+}
